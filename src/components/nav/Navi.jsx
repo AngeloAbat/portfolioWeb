341445cb1aa85a6f2a6920f26084606c242896f1
AngeloAbat <angeloabat25@gmail.com>
2022-12-24 13:50:59 -0800

    NavNameChange

--- src/components/nav/Navi.jsx
+++ src/components/nav/Navi.jsx
@@ -7,16 +7,20 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NaviBar(){
 
     const styles = {
-        background: 'linear-gradient(to right, #d40845 90%, #6F2232 200%)',
-        WebkitBackgroundClip: "text",
-        WebkitTextFillColor: 'transparent'
+        color: "#d40845",
+        borderbottom: '0px',
+        // background: 'linear-gradient(to right, #d40845 90%, #6F2232 200%)',
+        // WebkitBackgroundClip: "text",
+        // WebkitTextFillColor: 'transparent'
     }
 
     return (
         <div>
             <Navbar bg="dark" variant="dark" style={{position: "fixed", width: '100%', height: '60px'}}>
                 <Container>
-                <Navbar.Brand href="/" style={styles}>Angelo Abat</Navbar.Brand>
+                <Navbar.Brand href="/" style={styles}>
+                    <h4>Angelo Abat</h4>
+                </Navbar.Brand>
                     <Nav className="me-2">
                         <Nav.Link href="/AboutMe">About Me</Nav.Link>
                         <Nav.Link href="/Skills">Skills</Nav.Link>
