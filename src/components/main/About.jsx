d5ae4d9182e6f8890378787fea5e2adb998edde9
AngeloAbat <angeloabat25@gmail.com>
2023-01-09 21:06:50 -0800

    Added size Nav

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -2,7 +2,7 @@
 export default function About(){
     return(
         <div id="About" className=" borders">
-            <h1 className="Headers">About Me!</h1>
+            <h1  className="Headers">About Me!</h1>
             <br/>
             <p>I'm a Junior Web Developer seeking for a position where I can apply and hone my Web Development skills. Working in multiple projects helped me gain confidence and experience in creating an efficient and strong website.</p>
         </div>
