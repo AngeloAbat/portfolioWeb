a0182828a9a6cc22e1c89d39e954d70c7fa17aba
AngeloAbat <angeloabat25@gmail.com>
2023-08-30 13:25:27 -0700

    Landing Update

--- src/public/sections/Landing.jsx
+++ src/public/sections/Landing.jsx
@@ -4,6 +4,8 @@ export default function LandingSection(){
             <h1>
                 My name is Angelo Abat and this website is still under construction.
             </h1>
+            <br/>
+            <p>But Feel free to browse through</p>
         </div>
     )
 }
