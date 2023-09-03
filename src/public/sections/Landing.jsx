b4be3644067eca0f9335f767d473e18ffe51584c
AngeloAbat <angeloabat25@gmail.com>
2023-09-02 21:16:35 -0700

    Update 2

--- src/public/sections/Landing.jsx
+++ src/public/sections/Landing.jsx
@@ -4,7 +4,6 @@ let Me = require('../img/me.jpg')
 export default function LandingSection(){
     return(
         <div className="sectionFiller sectionContainer">
-            <img className='picture' src={Me} alt='A picture of Me'/>
             <img className='picture' src={Astronaut} alt='An Astronaut'/>
             <h1>
                 My name is Angelo Abat and this website is still under construction.
